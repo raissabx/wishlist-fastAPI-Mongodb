05b3c20b7a6224c3253149b565144a195abc6c84
raissa.xavier <raissa.xavier@luizalabs.com>
2023-11-28 15:15:57 -0300

    routes

---
+++ routes/route_customer.py
@@ -0,0 +1,150 @@
+from typing import Dict
+from fastapi import APIRouter, HTTPException
+from models.models import CustomerModel
+from config.database import collection_customer, collection_product
+from bson.objectid import ObjectId
+
+
+
+router = APIRouter()
+
+@router.post(
+        '/customer',
+        tags=['customer'],
+        summary = 'Cadastrar um cliente',
+        response_model = CustomerModel
+)
+async def create_customer(customer: CustomerModel):
+    customer_dict = customer.dict()
+    
+    if collection_customer.find_one({'email': customer_dict['email']}):
+        raise HTTPException(status_code=400, detail='Customer already exists')
+    
+    collection_customer.insert_one(customer_dict)
+    return customer_dict
+
+
+@router.get(
+        '/customer',
+        tags=['customer'], 
+        summary = 'Consultar todos os clientes',
+        response_model = list[CustomerModel]
+)
+async def get_all_customer():
+    cursor = collection_customer.find()
+    customer_list = list(cursor)
+
+    return customer_list
+
+@router.get(
+        '/customer/{id}',
+        tags=['customer'],
+        summary = 'Consultar cliente por id',
+        response_model = CustomerModel
+)
+async def get_customer(id: str):
+    customer = collection_customer.find_one({'_id': ObjectId(id)})
+    if not customer:
+        raise HTTPException(status_code=404, detail='Customer not found')
+    
+    return customer
+
+@router.put(
+        '/customer/{id}',
+        tags=['customer'],
+        summary = 'Atualizar cliente pelo ID',
+        response_model = CustomerModel
+)
+async def update_customer(customer: CustomerModel, id: str):
+    customer_dict = customer.dict()
+    object_id = ObjectId(id)
+
+    customer_in_db = collection_customer.find_one(
+        {'_id': object_id}
+    )
+    if not customer_in_db:
+        raise HTTPException(status_code=404, detail='Customer not found')
+    
+    if customer_in_db["email"] != customer_dict["email"]:
+        customer_with_new_email = collection_customer.find_one(
+            {"email": customer_dict["email"]}
+        )
+        if customer_with_new_email:
+            raise HTTPException(status_code=400, detail='Email already in use')
+        
+    collection_customer.update_one({'_id': object_id}, {'$set': customer_dict})
+    return customer_dict
+    
+@router.delete(
+    '/customer/{id}',
+    tags=['customer'],
+    summary = 'Deletar cliente',
+    response_model = Dict[str, str]
+)
+async def delete_customer(id: str):
+    customer = collection_customer.find_one_and_delete({"_id": ObjectId(id)})
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    return {"detail": "Customer deleted successfully"}
+
+
+@router.put(
+        '/customer/{customer_id}/add_favorite/{name_product}',
+        tags=['wishlist'],
+        summary = 'Lista de Favoritos',
+        response_model = Dict[str, str]
+)
+async def add_favorite(customer_id: str, name_product: str):
+    customer = collection_customer.find_one({'_id': ObjectId(customer_id)})
+    if not customer:
+        raise HTTPException(status_code=404, detail='Customer not found')
+
+    product = collection_product.find_one({'name_product': name_product})
+    if not product:
+        raise HTTPException(status_code=404, detail='Product not found')
+    
+    if name_product in customer['favorites']:
+        raise HTTPException(status_code= 400, detail='Product already in favorites list')
+    
+    collection_customer.update_one({'_id': ObjectId(customer_id)}, {'$push': {'favorites': name_product}})
+    return {'detail': 'Product added to favorites successfully'}
+
+
+@router.delete(
+        '/customer/{customer_id}/',
+        tags=['wishlist'],
+        summary = 'Deletar todos os itens da lista de favoritos',
+        response_model = Dict[str, str]
+)
+async def remove_all_favorite(customer_id: str):
+    customer = collection_customer.find_one({'_id': ObjectId(customer_id)})
+    if not customer:
+        raise HTTPException(status_code=404, detail="Customer not found")
+    
+    collection_customer.update_one({'_id': ObjectId(customer_id)}, {'$set': {'favorites': []}})
+    return {'detail': 'Favorites list cleared successfully'}
+
+@router.delete(
+    '/customer/{customer_id}/remove_favorite/{name_product}',
+    tags=['wishlist'],
+    summary= 'Remover um item da lista de favoritos',
+    response_model = Dict[str, str]
+)
+async def remove_favorite(customer_id: str, name_product: str):
+    customer = collection_customer.find_one({'_id': ObjectId(customer_id)})
+    if not customer:
+        raise HTTPException(status_code=404, detail= 'Customer not found')
+    
+    product = collection_product.find_one({'name_product': name_product})
+    if not product:
+        raise HTTPException(status_code= 404, detail= 'Product not found')
+    
+    if name_product not in customer['favorites']:
+        raise HTTPException(status_code=400, detail='Product not in favorites list')
+    
+    collection_customer.update_one({'_id': ObjectId(customer_id)}, {'$pull': {'favorites': name_product}})
+    return {'detail': 'Product removed from favorites successfully'}
+
+
+
+
---
+++ routes/route_product.py
@@ -0,0 +1,52 @@
+from typing import Dict
+from fastapi import APIRouter, HTTPException
+from models.models import ProductModel
+from config.database import collection_product
+from bson.objectid import ObjectId
+
+
+router = APIRouter()
+
+@router.post(
+        '/product',
+        tags = ['product'],
+        summary = 'Cadastrar produto',
+        response_model = ProductModel
+)
+async def create_product(product: ProductModel):
+    product_dict = product.dict()
+    if collection_product.find_one(
+        {'name_product': product_dict['name_product']}
+    ):
+        raise HTTPException(status_code=400, detail='Product already exists')
+    
+    collection_product.insert_one(product_dict)
+
+    return product_dict
+
+
+@router.get(
+        '/product',
+        tags = ['product'],
+        summary = 'Consultar todos os produtos',
+        response_model = list[ProductModel]
+)
+async def get_product():
+    cursor = collection_product.find()
+    product_list = list(cursor)
+
+    return product_list
+
+
+@router.delete(
+    '/product/{name_product}',
+    tags = ['product'],
+    summary = 'Deletar produto',
+    response_model = Dict[str, str]
+)
+async def delete_product(name_product: str):
+    product = collection_product.find_one_and_delete({"name_product": name_product})
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    
+    return {"detail": "Product deleted successfully"}
