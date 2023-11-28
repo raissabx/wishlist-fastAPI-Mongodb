102e252dc89c0326cbb66fd1b83b197b3af3533f
rai_xavier <raissa.xavier@luizalabs.com>
2023-11-28 10:38:25 -0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from fastapi import FastAPI
+from routes.route_customer import router as router_customer
+from routes.route_product import router as router_product
 
 app = FastAPI(title='Desafio Wishlist')
+app.include_router(router_customer)
+app.include_router(router_product)
+
+
+
