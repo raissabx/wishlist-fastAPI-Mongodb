291605b2b047e2d1c09c6d7ae6d68ba84ef8cd03
rai_xavier <raissa.xavier@luizalabs.com>
2023-11-28 10:37:02 -0300

    database

--- config/database.py
+++ config/database.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://raissabxdev:201295@cluster0.xcetukc.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb://localhost:27018")
+db = client['API_Wishlist']
 
-db = client['API Wishlist']
-
-collection_client = db['client_collection']
+collection_customer = db['customer_collection']
 collection_product = db['product_collection']
 
