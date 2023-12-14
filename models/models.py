2f0bf3d9833a6b469980d55cdd228a14d92c1e71
raissa.xavier <raissa.xavier@luizalabs.com>
2023-12-14 10:23:00 -0300

    models_

--- models/models.py
+++ models/models.py
@@ -1,16 +1,18 @@
-from typing import Annotated, List, Optional
+from typing import Annotated, Optional
 from pydantic import BaseModel, Field, BeforeValidator
-
 
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 
 class CustomerModel(BaseModel):
-    id: Optional[PyObjectId] = Field(alias="_id", default=None)
     name_customer: str
     email: str
     favorites: list = []
+
+
+class CustomerOut(CustomerModel):
+    id: str = Field(title='ID of customer')
 
 
 class ProductModel(BaseModel):
